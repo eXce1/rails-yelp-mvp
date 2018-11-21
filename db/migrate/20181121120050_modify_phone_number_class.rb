5296d1eaf0dc7da9356ca401a64788f1821a0a21
Damien Delahaye <damien.delahaye2@gmail.com>
2018-11-21 17:31:19 +0100

    exo2presquefini

---
+++ db/migrate/20181121120050_modify_phone_number_class.rb
@@ -0,0 +1,5 @@
+class ModifyPhoneNumberClass < ActiveRecord::Migration[5.1]
+  def change
+    change_column :restaurants, :phone_number, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Bombay',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0132569874',
+    category:     'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '3652987401',
+    category:     'italian'
+  },
+   {
+    name:         'Dikkenek',
+    address:      '7 avenue de la grosse frite',
+    phone_number: '85639677425',
+    category:     'belgian'
+  },
+  {
+    name:         'Le omelette du fromage',
+    address:      '56 baguette street, Paris',
+    phone_number: '7452963602',
+    category:     'french'
+  },
+   {
+    name:         'Chingchong',
+    address:      '7 rue du ping pong China',
+    phone_number: '1458628502',
+    category:     'chinese'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
